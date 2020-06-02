9dd216538f9b4cf0a064e568a8aae0f3cfd9375a
Usman <Murtala>
2020-06-02 01:48:27 +0100

    created header and smoothie card component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
 
-<style>
+<script>
+import Navbar from "@/components/Navbar";
+export default {
+  name: "App",
+  components: {
+    Navbar
+  }
+};
+</script>
 
-</style>
+<style></style>
